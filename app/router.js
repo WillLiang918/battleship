08859add316bc7cdcfdca94d2a5faaed6d6acaf9
William Liang <williamliang918@gmail.com>
2016-11-15 20:44:06 -0500

    add adapter and routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', {path: '/'});
+  this.resource('games', {path: 'games/:game_id'});
 });
 
 export default Router;
